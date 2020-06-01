24fcac4a3b70892c2b6917696a3691d8afe040e5
严俊东 <1172482914@qq.com>
2020-06-01 16:16:43 +0800

    删除无用接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,6 @@
 import http from './http'
 import store from '../store'
 
-/** 枚举接口 */
-export const enumsAdd = params => http.post(`/api/enums`, params)
-export const enumsDel = id => http.delete(`/api/enums/${id}`)
-export const enumsModify = (id, params) => http.put(`/api/enums/${id}`, params)
-export const enumsDetail = id => http.get(`/api/enums/${id}`)
-export const enumsList = params => http.get(`/api/enums`, params)
 /** 获取所有枚举（简化字段） */
 export const enumsTotal = params => http.get('/api/enums/enumsTotal', params)
 
